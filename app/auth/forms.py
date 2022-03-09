cfc57ed5a4d5dfd56b96eb0745187aea454b0adc
silvianjoki <10silvianjoki@gmail.com>
2022-03-09 04:45:49 +0300

    add login query

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -16,7 +16,6 @@ class RegistrationForm(FlaskForm):
     
 
 # custom validators.
-
     # .......
     def validate_email(self,data_field):
             if User.query.filter_by(email =data_field.data).first():
@@ -25,3 +24,10 @@ class RegistrationForm(FlaskForm):
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
+        
+        
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,30 @@
 from flask import render_template
 from . import main
-from flask_login import login_required
+from flask_login import login_required, current_user, login_user, logout_user
 
 
 # @main.route('/login', methods = ['GET','POST'])
 # @login_required
 # def login():
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.username.data).first()
+#         if user is None or not user.check_password(form.password.data):
+#             flash('Invalid username or password')
+#             return redirect(url_for('login'))
+#         login_user(user, remember=form.remember_me.data)
+#         return redirect(url_for('index'))
+#     return render_template('login.html', title='Sign In', form=form)
+
+
+
+# @app.route('/logout')
+# def logout():
+#     logout_user()
+#     return redirect(url_for('index'))
 
