509eb34e53e1fea4fd84a3538e3035f37b15943b
silvianjoki <10silvianjoki@gmail.com>
2022-03-09 05:01:29 +0300

    add user func

--- app/main/views.py
+++ app/main/views.py
@@ -19,7 +19,24 @@ def profile(uname):
 # @login_required
 # def login():
 
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
 
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
 
 # @app.route('/login', methods=['GET', 'POST'])
 # def login():
