e293893c8216c060957c95e715f6031b1e047672
silvianjoki <10silvianjoki@gmail.com>
2022-03-09 11:13:47 +0300

    add images flask upload

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+
 
 # initializing bootstrap extension
 bootstrap = Bootstrap()
@@ -10,6 +12,8 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
+
 
 
 
